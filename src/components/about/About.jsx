f5069ccc48ab7430e43e6f55fa9ac2ebbddc230f
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2022-02-19 13:57:53 +0000

    Import remark gfm

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,18 +2,19 @@ import classes from './About.module.scss';
 import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown'
 import { ExampleHouse } from '../../houses'
+import RemarkGfm from 'remark-gfm'
 
 export default function About() {
   const [house, setHouse] = useState(null)
   useEffect(() => {
     fetch(ExampleHouse).then(res => res.text()).then(text => setHouse(text));
   }, [])
-  console.log(ExampleHouse.md)
+
   return (
     <div className={classes.aboutContainer}>
       <div className={classes.title}>about</div>
       <div className={classes.contentContainer}>
-      <ReactMarkdown children={house}/>
+      <ReactMarkdown children={house} remarkPlugins={[RemarkGfm]}/>
         This is an example about component
       </div>
     </div>
